d1359ac3f8fc5ea517a44eb3445acf6113492941
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2025-02-27 16:54:43 +0600

    Some basic updates in django backend before deployment.

--- django_backend_connectsphere/connectsphere_backend/settings.py
+++ django_backend_connectsphere/connectsphere_backend/settings.py
@@ -174,8 +174,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = os.getenv('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  
 
-MEDIA_URL = '/media/'
+MEDIA_URL = os.getenv('MEDIA_ROOT')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
